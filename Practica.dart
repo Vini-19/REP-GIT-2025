62241f2e01b16543b2ea1e5d354cbe9afa3e3aea
Vini-19 <marcosanchez-1@outlook.com>
2025-05-20 17:51:56 -0600

    subir nuevas lineas de codigo de operadores logicos

--- Practica.dart
+++ Practica.dart
@@ -52,4 +52,36 @@ is! = compara si el valor de los datos son distintos
 
   var re = 30 == 15;
   print(re);
+  /*> mayor que*/
+  var rema = 30 > 15;
+  print(rema);
+
+  /*< menor que*/
+  var remeno = 30 < 15;
+  print(remeno);
+
+  /*== igual*/
+  var reigual = 30 == 15;
+  print(reigual);
+
+  /*<= menor igual*/
+  var remenoigual = 30 <= 15;
+  print(remenoigual);
+
+  /*>= igual*/
+  var reigualma = 30 >= 15;
+  print(reigualma);
+
+  /* is = compara el valor de los datos*/
+  var reisigua = 30 is int; // ¿30 es un int?
+  print(reisigua); // true
+
+  /* is! = compara si el valor de los datos son distintos*/
+
+  var reiss = 30 is! String; // ¿30 NO es un String?
+  print(reiss); // true
+
+  /* != si son diferentes*/
+  var reiis = 30 != 15;
+  print(reiis);
 }
