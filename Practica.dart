7d10c4bc34d56c3bdad5c377b48dfb5793ca1990
Vini-19 <marcosanchez-1@outlook.com>
2025-05-19 20:19:19 -0600

    first commit

---
+++ Practica.dart
@@ -0,0 +1,37 @@
+void main() {
+  int entero = 2025;
+  print(entero);
+
+  String nombre = "Vinicio";
+  print(nombre);
+
+  dynamic dato = 20;
+  print(dato);
+
+  double decimal = 20.5;
+  print(decimal);
+
+  bool valor = true;
+  print(valor);
+
+  //saidas
+
+  print("\n Nombre es: " + "$nombre" + "\n Numero Decimal es: " + "$decimal");
+
+  //operaciones aritmeticas
+
+  int x = 25;
+  int y = 20;
+
+  dynamic resultado = x + y;
+  print("su suma es: $resultado");
+
+  dynamic resoltadoResta = x - y;
+  print("su resta es: $resoltadoResta");
+
+  dynamic resultadoMultiplicacion = x * y;
+  print("su multiplicacion es: $resultadoMultiplicacion");
+  //division
+  dynamic resultadoDivision = x / y;
+  print("su division es: $resultadoDivision");
+}
